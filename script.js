2eb2dd1bdb27dafca7659dd4bd3b219e58c8c492
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2020-10-11 12:53:59 -0400

    js updates, working with HTML to display search results

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function getCityName() {
-    var cityName = document.querySelector("#cityName").value;
+    var cityName = document.querySelector("#cityname").value;
     getWeatherData(cityName);
     makeRow(cityName);
 }
@@ -49,7 +49,7 @@ var getWeatherData = function(cityname) {
         var cardBodyEl = document.createElement("div");
         cardBodyEl.classList.add("card-body");
         var imgEl = document.createElement("img");
-        imgEl.setAttribute("src", "http://openweathermap.org/img/w/" + data.weather.icon);
+        imgEl.setAttribute("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
 
         titleEl.appendChild(imgEl)
         cardBodyEl.appendChild(titleEl);
@@ -63,4 +63,91 @@ var getWeatherData = function(cityname) {
         getUVIndex(data.coord.lat, data.coord.lon);
     }
 )}
-  
+
+function getForecast(cityName) {
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&appid=ed3ceecb82da99a626e9f6aef02e2dbb&units=imperial")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data)
+        var forecastEl = document.querySelector("#forecast");
+        forecastEl.innerHTML = "<h4 class=\"mt-3\">5-Day Forecast:</h4>";
+        forecastRowEl.className = "\"row\"";
+
+        //loop over all forecasts in 3-hour increments
+        for (var i = 0; i <data.list.length; i++) {
+            
+            //only look at forecasts around 3pm
+            if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+
+                //create html elements for a bootstrap card
+                var colEl = document.createElement("div");
+                colEl.classList.add("col-md-2");
+                var cardEl = document.createElement("div");
+                cardEl.classList.add("card", "bg-primary", "text-white");
+                var windEl = document.createElement("p");
+                windEl.classList.add("card-text");
+                windEl.textContent = "Wind Speed: " + data.list[i].wind.speed + " MPH";
+                var humidityEl = document.createElement("p");
+                humidityEl.classList.add("card-text");
+                humidityEl.textContent = "Humidity: " + data.list[i].main.humidity + " %";
+                var bodyEl = document.createElement("div");
+                bodyEl.classList.add("card-body", "p-2");
+                var titleEl = document.createElement("h5");
+                titleEl.classList.add("card-title");
+                titleEl.textContent = new Date(data.list[i].dt_txt).toLocaleDateString()
+                var imgEl = document.createElement("img")
+                imgEl.setAttribute("src", "http://openweathermap.org.img/w/" + data.list[i].weather[0].icon + ".png" )
+                var p1El = document.createElement("p");
+                p1El.classList.add("card-text");
+                p1El.textContent = "Temp: " + data.list[i].main.temp_max + " °F";
+                var p2El = document.createElement("p");
+                p2El.classList.add("card-text");
+                p2El.textContent = "Humidty: " + data.list[i].main.humidity + "%";
+
+                //merge together and display on page
+                colEl.appendChild(cardEl);
+                bodyEl.appendChild(titleEl);
+                bodyEl.appendChild(imgEl);
+                bodyEl.appendChild(windEl);
+                bodyEl.appendChild(humidityEl);
+                bodyEl.appendChild(p1El);
+                bodyEl.appendChild(p2El);
+                cardEl.appendChild(bodyEl);
+                forecastEl.appendChild(colEl);
+
+            }
+        }
+    });
+}
+
+function getUVIndex(lat, lon) {
+    fetch("https://api.openweathermap.org/data/2.5/uvi?appid=ed3ceecb82da99a626e9f6aef02e2dbb&lat=" + lat + "&lon=" + lon)
+    .then(function(response){
+        return response.json();
+    }).then(function(data) {
+        var bodyEl = document.querySelector(".card-body");
+        var uvEl = document.createElement("p");
+        uvEl.textContent = "UV Index: "
+        var buttonEl = document.createElement("span");
+        buttonEl.classList.add("btn", "btn-sm");
+        buttonEl.innerHTML = data.value;
+
+        if (data.value < 3) {
+            buttonEl.classList.add("btn-success");
+        }
+        else if (data.value < 7) {
+            buttonEl.classList.add("btn-warning");
+        }
+        else {
+            buttonEl.classList.add("btn-danger");
+        }
+
+        bodyEl.appendChild(uvEl);
+        uvEl.appendChild(buttonEl);
+    })
+}
+
+document.querySelector("#search-button").addEventListener("click", getCityName);
+    
